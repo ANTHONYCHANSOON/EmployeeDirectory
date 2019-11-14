bd28ee71b87abe892b16f7c42e7344d284b21163
anthony soon <anthonychansoon@gmail.com>
2019-11-14 09:52:11 -0800

    DataArea and EmpCOntext

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import "../styles/Header.css";
 
-export default class Header extends Component {
-  render() {
+
+//export default class Header extends Component {
+
+function Header() {
+  
     return (
       <div className="header">
         <h1>Employee Directory</h1>
         <p>Click on carrots to filter by heading or use the search box to narrow your results.</p>
       </div>
     )
-  }
+  
 }
+
+export default Header;
---
+++ src/utils/EmpContext.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const EmpContext = React.createContext ({
+    users: "",
+    order: "decend",
+    filteredUsers: [{}],
+    headings: [
+      { name: "Image", width: "10%" },
+      { name: "Name", width: "10%" },
+      { name: "Phone", width: "20%" },
+      { name: "Email", width: "20%" },
+      { name: "DOB", width: "10%" }
+    ]
+});
+
+export default EmpContext;
